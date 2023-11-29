7b804cea97adf8bdad1240cbe1a162b2ecfe1c00
Maria Angeles <maria.d.juri@gmail.com>
2023-11-28 19:51:54 -0500

    Got book titles to show up on the page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css'
-import bookData from './book-data.json'
+import "./App.css";
+import bookData from "./book-data.json";
 
 function App() {
   return (
     <div>
       <h1>Freeshelf</h1>
-      {/* Use bookData to show books on the page*/}
+      <div className="titles">
+        {bookData.map((book) => {
+          return book.title;
+        })}
+        {/* how do we get all the book titles to display vertically */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
