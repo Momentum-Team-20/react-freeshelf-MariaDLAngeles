3f340017ccadb6ae3e328a7f80417f1c0b994da9
Maria Angeles <maria.d.juri@gmail.com>
2023-11-29 20:42:22 -0500

    aria-expanded is working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
 import bookData from "./book-data.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+//trying sorting from:
+// https://plainenglish.io/blog/sorting-react-components-using-sort
+
+// const [bookData, setBookData] = useState([])
+
+// useEffect(() => {
+//   setBookData(props.bookData)
+// }, [])
+
+// function handleSort() {
+//   const sortedData = [...bookData].sort((a,b) => {
+//     return a.first > b.first ? 1 : -1
+//   })
+//   setBookData(sortedData)
+// }
 
 function App() {
   return (
     <div>
       <div className="bookDetail">
+        {/* the button below is to try the sorting option */}
+        {/* <button onClick={handleSort} id="sort-a-z">Sort from A-Z</button> */}
+        <br></br>
         {bookData.map((book) => (
           <Book
           // key ={unique.id} -- this would solve the unique key error in the console, but our data doesn't have a unique key
@@ -26,15 +45,16 @@ function App() {
 
 // this is our react component
 function Book(props) {
-  const [expanded, setExpanded] = useState(false);
+  const [expanded, setExpanded] = useState(false); // on component creation, expanded === false
   const handleClick = () => {
     setExpanded(!expanded);
   };
-  const handleImageError = fallback => event => event.target.src = fallback;
+  const fallBackImage = "images/no_cover_image.jpg"
+  const handleImageError = event => event.target.src = fallBackImage;
   // const fallBackImage = 'https://hips.hearstapps.com/hmg-prod/images/lonely-pug-royalty-free-image-1652974264.jpg?crop=0.447xw:1.00xh;0.355xw,0&resize=980:*'
 
-  const fallBackImage = "images/no_cover_image.jpg"
   const placeHolderStatement = 'NOT AVAILABLE'
+
 
   /**
    * props: {
@@ -50,10 +70,10 @@ function Book(props) {
       <h1 className="title">{props.title}</h1>
       <h3 className="author"><strong>Author:</strong> {props.author}</h3>
       <p className="description"><strong>Description:</strong> {props.shortDescription}</p>
-      <img src={props.coverImageUrl} onError={handleImageError(fallBackImage)} alt="book cover" className="coverImage" />
+      <img src={props.coverImageUrl} onError={handleImageError} alt="book cover" className="coverImage" />
       {/* <img src={props.coverImageUrl ? props.coverImageUrl : fallBackImage} onError={addDefaultSrc} alt="book cover" className="coverImage" /> */}
       <br></br>
-      <button onClick={handleClick}>
+      <button aria-expanded={expanded} onClick={handleClick}>
         {expanded ? "Show Less" : "Show More"}
       </button>
       {expanded && (
